d75c7d8f358a00235d550348e6c0a9a0e938c79b
IvanaGoSt <ivanagost@zoho.com>
2021-03-23 22:11:41 +0100

    Add eng button and links to local inages

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+let srp = document.getElementsByClassName("srp");
+let eng = document.getElementsByClassName("eng");
+let btn = document.getElementById("lang")
+
+function changeLanguage() {
+  for(let i = 0; i < srp.length; i++) {
+    if(eng[i].style.display === "none") {
+      srp[i].style.display = "none";
+      eng[i].style.display = "block";
+      btn.innerHTML = "srp";
+    } else {
+      eng[i].style.display = "none";
+      srp[i].style.display = "block";
+      btn.innerHTML = "eng"
+    }
+  }
+}
